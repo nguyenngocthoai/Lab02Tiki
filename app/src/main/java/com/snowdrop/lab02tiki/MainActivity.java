8b9c745716004faddecec10d85275ae88540f26f
ThoaiVocie <thoainguyen603@gmail.com>
2020-10-13 15:17:46 +0700

    Add function to increase or decrease the number. Discount code applied

--- app/src/main/java/com/snowdrop/lab02tiki/MainActivity.java
+++ app/src/main/java/com/snowdrop/lab02tiki/MainActivity.java
@@ -8,13 +8,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton btnCong, btnTru;
-    TextView txtSoLuong;
+    TextView txtSoLuong, txtPrice, txtSUmPrice, txtPriceOrginal;
     int soluong = 1;
     Button btnApDung;
-    EditText editMaGiamGia;
+    EditText edtMaGiamGia;
 
 
     @Override
@@ -24,13 +25,18 @@ public class MainActivity extends AppCompatActivity {
 
         btnCong = findViewById(R.id.btnCong);
         btnTru = findViewById(R.id.btnTru);
-        txtSoLuong=findViewById(R.id.txtSoLuong);
-        editMaGiamGia=findViewById(R.id.edtMaGiamGia);
+        txtSoLuong = findViewById(R.id.txtSoLuong);
+        edtMaGiamGia = findViewById(R.id.edtMaGiamGia);
+        btnApDung = findViewById(R.id.btnApDung);
+        txtPrice = findViewById(R.id.txtPrice);
+        txtSUmPrice = findViewById(R.id.txtSumPrice);
+        txtPriceOrginal = findViewById(R.id.txtPriceOrginal);
 
         txtSoLuong.setText(soluong + "");
         btnTru.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "clicked -", Toast.LENGTH_SHORT).show();
                 soluong = soluong - 1;
                 if (soluong < 1) {
                     soluong = 1;
@@ -41,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         btnCong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "clicked+", Toast.LENGTH_SHORT).show();
                 soluong = soluong + 1;
                 txtSoLuong.setText(soluong + "");
             }
@@ -49,7 +56,14 @@ public class MainActivity extends AppCompatActivity {
         btnApDung.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (edtMaGiamGia.getText().toString().trim().equals("thoai")) {
+//                    Toast.makeText(MainActivity.this, edtMaGiamGia.getText().toString().trim(), Toast.LENGTH_SHORT).show();
+                    txtSUmPrice.setText("100.000 đ");
+                    txtPrice.setText("100.000 đ");
+//                    txtPriceOrginal.setText("100.000 đ");
+                } else {
+//                    Toast.makeText(MainActivity.this, "Mã giảm giá không hợp lệ !", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
